c340f53988bc56bde02e4a38ee55ac6d8f3335fd
Alejandra Pinto <consuelo.pinto02@inacapmail.cl>
2025-05-20 23:56:51 -0400

    Japanese_Cities_Attributes.sql

--- Japanese_Cities_Attributes.sql
+++ Japanese_Cities_Attributes.sql
@@ -1,1 +1,5 @@
 SELECT * FROM CITY WHERE COUNTRYCODE='JPN';
+
+Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+The CITY table is described as follows:
