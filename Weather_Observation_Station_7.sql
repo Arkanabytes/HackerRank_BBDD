19cf2515e5978dfae2f6d7eb04fa95ccb0bc508e
Alejandra Pinto <consuelo.pinto02@inacapmail.cl>
2025-05-29 23:14:10 -0400

    Weather_Observation_Station_7.sql

--- Weather_Observation_Station_7.sql
+++ Weather_Observation_Station_7.sql
@@ -3,7 +3,6 @@ FROM STATION
 WHERE CITY REGEXP '[aeiouAEIOU]$';
 
 Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
-
 Input Format
 
 The STATION table is described as follows:
