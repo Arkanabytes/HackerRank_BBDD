df4fc141ce259574b7c4b2ff613942888b8b8f89
Alejandra Pinto <consuelo.pinto02@inacapmail.cl>
2025-05-29 23:13:56 -0400

    Weather_Observation_Station_7.sql

--- Weather_Observation_Station_7.sql
+++ Weather_Observation_Station_7.sql
@@ -1,3 +1,9 @@
 SELECT DISTINCT CITY
 FROM STATION
 WHERE CITY REGEXP '[aeiouAEIOU]$';
+
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
