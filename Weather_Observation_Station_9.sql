ef32da770a248d414fc90b95d3c2bda51ed512fa
Alejandra Pinto <consuelo.pinto02@inacapmail.cl>
2025-05-30 23:40:47 -0400

    Weather_Observation_Station_9.sql

--- Weather_Observation_Station_9.sql
+++ Weather_Observation_Station_9.sql
@@ -3,6 +3,13 @@ select distinct city from station where NOT REGEXP_LIKE (UPPER(city),'^[AEIOU].*
 
 /*
 
+Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
+
 Given a table STATION that holds data for five fields namely ID, CITY, STATE, NORTHERN LATITUDE and WESTERN LONGITUDE.
 +-------------+------------+
 | Field       |   Type     |
@@ -18,10 +25,3 @@ Write a query to print the list of CITY that does not start with vowels in lexic
 
 */
 
-
-
-Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
-
-Input Format
-
-The STATION table is described as follows:
