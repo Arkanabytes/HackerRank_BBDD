533bcdbb17fecb0a61df292f193359300f734e1e
Alejandra Pinto <consuelo.pinto02@inacapmail.cl>
2025-05-30 23:39:29 -0400

    Weather_Observation_Station_9.sql

--- Weather_Observation_Station_9.sql
+++ Weather_Observation_Station_9.sql
@@ -1,6 +1,24 @@
 
 
 
+/*
+
+Given a table STATION that holds data for five fields namely ID, CITY, STATE, NORTHERN LATITUDE and WESTERN LONGITUDE.
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+ 
+Write a query to print the list of CITY that does not start with vowels in lexicographical order. Do not print duplicates.
+
+*/
+
+
 
 Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
 
