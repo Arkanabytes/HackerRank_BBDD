80cd047b34b92d0e95a27e02f34ddb82d7a0ff30
Alejandra Pinto <consuelo.pinto02@inacapmail.cl>
2025-06-05 00:00:06 -0400

    Update Weather_Observation_Station_6.sql

--- Weather_Observation_Station_6.sql
+++ Weather_Observation_Station_6.sql
@@ -5,3 +5,12 @@ WHERE CITY REGEXP '^[aeiouAEIOU]';
 Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
 Input Format
 The STATION table is described as follows:
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
